ad749936db8c535891e7b55e1fdf79e641712fc6
EngrChike <134385611+EngrChike@users.noreply.github.com>
2026-05-06 03:55:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 // 1. YOUR SUPABASE CONNECTION
 
 // Put your address inside the first set of quotes
+const supabaseUrl = 'https://opszvifybrteqdfozbkr.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wc3p2aWZ5YnJ0ZXFkZm96YmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTQ5MzQsImV4cCI6MjA5MzU5MDkzNH0.cToJ5sLDcXGgDfJS2o_Ww-fwb69FaUgS4rriQfiGjeI'; 
 
-const _supabaseUrl = 'https://opszvifybrteqdfozbkr.supabase.co'; 
+// CRITICAL: Look at the underscore (_) before the word supabase!
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-
-// Put your long 'Anon Key' (from your Supabase dashboard) inside these quotes
-
-const _supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wc3p2aWZ5YnJ0ZXFkZm96YmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTQ5MzQsImV4cCI6MjA5MzU5MDkzNH0.cToJ5sLDcXGgDfJS2o_Ww-fwb69FaUgS4rriQfiGjeI'; 
 
 
 // 2. THE APP'S MEMORY (Bookmarks for editing)
